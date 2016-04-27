393970d84bae679220304c3efcfa975a6e4f50b1
Jared Levin <jaredrlevin@gmail.com>
2016-04-27 12:26:58 -0700

    second commit

---
+++ db/migrate/20160427154401_pizza.rb
@@ -0,0 +1,13 @@
+class Pizza < ActiveRecord::Migration
+  def change
+  	create_table :pizzas do |t|
+  		t.string	:size
+  		t.string	:crust
+  		t.string	:sauce
+  		t.text	:toppings
+  		t.string	:cheese
+
+  		t.timestamps
+  	end
+  end
+end
